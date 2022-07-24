2476a77aaabf3f57f6372a2e2b53127121ae31d2
ibtisam <iuhmirza@gmail.com>
2022-07-24 16:03:01 +0100

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n < 0) {
+        return "OOPS"
+    }
+    fibonacciArray = [1, 1];
+    for(let i = 0; i < n; i++) {
+        fibonacciArray[i+2] = fibonacciArray[i+1] + fibonacciArray[i];
+    }
+    return fibonacciArray[n-1]
 };
 
 // Do not edit below this line
