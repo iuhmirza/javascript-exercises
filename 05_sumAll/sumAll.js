64e275b492f6773a53eab9fd1806ee1bf394922a
ibtisam <iuhmirza@gmail.com>
2022-07-24 14:04:32 +0100

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0) {
+        return "ERROR"
+    }
+    if(typeof(a) !== "number" || typeof(b) !== "number") {
+        return "ERROR"
+    }
+    let start, stop;
+    if(b>a) {
+        start = a;
+        stop = b;
+    } else {
+        start = b;
+        stop = a;
+    }
+    let counter = 0;
+    for(let i = start; i < stop+1; i++) {
+        counter += i;
+    }
+    return counter;
 };
 
 // Do not edit below this line
