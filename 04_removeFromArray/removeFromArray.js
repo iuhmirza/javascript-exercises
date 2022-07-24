22e23205114afec2f34a70ec167ec2cbf0aa750a
ibtisam <iuhmirza@gmail.com>
2022-07-24 13:53:56 +0100

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...val) {
+    let removedArray = [];
+    let include = true;
+    for(let i = 0; i < array.length; i++) {
+        for(let j = 0; j < val.length; j++) {
+            if(array[i] === val[j]) {
+                include = false;
+            }
+        }
+        if(include === true){
+            removedArray.push(array[i]);
+        }
+        include = true
+    }
+    return removedArray;
 };
 
 // Do not edit below this line
