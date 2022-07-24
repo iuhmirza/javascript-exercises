14d53003ca612167efb93107bbf298cb3223e15f
ibtisam <iuhmirza@gmail.com>
2022-07-24 15:53:08 +0100

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let sanitisedString = "";
+    
+    for(let  i = 0; i < string.length; i++) {
+        if(string[i] !== "!" && string[i] !== "," && string[i] !== "." && string[i] !== " ") {
+            sanitisedString = sanitisedString + string[i];
+        }
+    }
+    sanitisedString = sanitisedString.toUpperCase()
+
+    let reversedString = "";
+
+    for(let i = sanitisedString.length-1; i >= 0; i--) {
+        reversedString = reversedString + sanitisedString[i];
+    }
+    if(reversedString === sanitisedString) {
+        return true;
+    } else {return false}
 
 };
 
