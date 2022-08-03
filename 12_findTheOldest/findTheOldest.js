923658450bad4e3aa20c1a6a07b8967797e44df2
ibtisam <iuhmirza@gmail.com>
2022-08-03 11:57:14 +0100

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    for(let person of people) {
+        if(person.yearOfDeath) {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person.age = (new Date().getFullYear()) - person.yearOfBirth;
+        }
 
+    };
+    let oldest = "";
+    let oldestAge = 0;
+    for(let person of people) {
+        if(person.age > oldestAge){
+            oldestAge = person.age;
+            oldest = person;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
